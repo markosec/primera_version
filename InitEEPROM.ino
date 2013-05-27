64db3cceb7d39202e015a2cf559a9d5aed24e53a
markosec <marcos.capra@gmail.com>
2013-05-27 08:42:13 -0300

    Update InitEEPROM.ino
    Correccion valores

--- InitEEPROM.ino
+++ InitEEPROM.ino
@@ -6,9 +6,9 @@ struct config_t
 {
   long m_totales_i;
   long m_totales_f;
-  int  acelerado;
-  int  desacelerado;
-  int  cuanto;    
+  int  acelerador;
+  int  cuanto_acel;    
+  int  cuanto_desacel;   
   int  burro;
   int  cebador;
   int  guardado;
@@ -19,15 +19,15 @@ struct config_t
 
 void setup()
 {
-  config.m_totales_i  = 40000;
-  config.m_totales_f  = 0;
-  config.acelerado    = 750;
-  config.desacelerado = 1000;
-  config.cuanto       = 90;
-  config.burro        = 1000;
-  config.cebador      = 90;
-  config.guardado     = 300000;
-  config.clave        = 1863;
+  config.m_totales_i    = 40000;
+  config.m_totales_f    = 0;
+  config.acelerador     = 90;
+  config.cuanto_acel    = 750;
+  config.cuanto_desacel = 750;
+  config.burro          = 1000;
+  config.cebador        = 90;
+  config.guardado       = 300000;
+  config.clave          = 1863;
     
   EEPROM_writeAnything(0, config);
   
